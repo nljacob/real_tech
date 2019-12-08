ed676d3427a80728084c16872035077aa44c647e
nljacob <neehaljacob@gmail.com>
2019-12-08 11:12:45 -0800

    connecting to mailchimp API

--- app.js
+++ app.js
@@ -2,24 +2,65 @@ const express = require('express');
 const request = require('request');
 const bodyParser = require('body-parser');
 const path = require('path');
-
 const app = express();
+const PORT = process.env.PORT || 3000;
+const MAILCHIMP_URL = process.env.MAILCHIMP_URL;
+const MAILCHIMP_AUTHORIZATION = process.env.MAILCHIMP_AUTHORIZATION;
 
-//Bodyparser Middleware
+// Bodyparser Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Static folder
+// Static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Signup Route
+app.post('/signup', (req, res) => {
+    const { firstName, number, email } = req.body;
+    console.log('First Name: ' + firstName + ' Number: ' + number + ' Email: ' + email);
 
-//Signup Route
-app.post('/signup', function (req, res) {
-    console.log(req.body)
-    res.send('hello world');
-})
+    // Make sure fields are filled
+    if (!firstName || !email) {
+        res.redirect('/fail.html');
+        return;
+    }
 
+    // Construct req data
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: 'subscribed',
+                merge_fields: {
+                    FNAME: firstName,
+                    EMAIL: email,
+                    NUMBER: number
+                }
+            }
+        ]
+    };
 
+    const postData = JSON.stringify(data);
 
-const PORT = process.env.PORT || 5000;
+    const options = {
+        url: MAILCHIMP_URL,
+        method: 'POST',
+        headers: {
+            Authorization: MAILCHIMP_AUTHORIZATION
+        },
+        body: postData
+    };
 
-app.listen(PORT, console.log(`Server started on ${PORT}`));
+    request(options, (err, response, body) => {
+        if (err) {
+            res.redirect('/fail.html');
+        } else {
+            if (response.statusCode === 200) {
+                res.redirect('/success.html');
+            } else {
+                res.redirect('/fail.html');
+            }
+        }
+    });
+});
+
+app.listen(PORT, console.log(`Server started on http://localhost:${PORT}`));
